6e5c8f166ef1fd8b8cf8e35c19df1eabc4bc6fff
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-04-03 17:47:46 +0300

    31. Validation in the API

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            {
+                ModelState.AddModelError("Username", "Username already exists");
+            }
+
+            // validate request
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userToCreate = new User{ Username = userForRegisterDto.Username };
+
+            var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
