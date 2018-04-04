ada1a869c3e71ac2678c01711597202ded8687d6
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-04-04 15:48:42 +0300

    34. Using the Authentication middleware

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
@@ -15,8 +16,10 @@ namespace DatingApp.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
@@ -37,7 +40,7 @@ namespace DatingApp.API.Controllers
             }
 
             var userToCreate = new User
-            { 
+            {
                 Username = userForRegisterDto.Username
             };
 
@@ -58,7 +61,7 @@ namespace DatingApp.API.Controllers
 
             // generate token
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("super secret key");
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
@@ -73,7 +76,7 @@ namespace DatingApp.API.Controllers
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var tokenString = tokenHandler.WriteToken(token);
 
-            return Ok( new {tokenString});
+            return Ok(new { tokenString });
         }
     }
 }
