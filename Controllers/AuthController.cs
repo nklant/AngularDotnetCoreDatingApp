46baa7b90a925204beed59d6473a5d354f7b4c8b
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-04-13 10:47:37 +0300

    56. Adding Ngx Bootstrap to power our Bootstrap components

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -55,7 +55,10 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
